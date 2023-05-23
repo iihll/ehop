e0b76646b6909acadec9b926b0fc491b0bd5b6ac
iihll <iihll.dev@gmail.com>
2023-05-23 23:42:47 +0800

    chore: create dev playground

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -4,19 +4,33 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import {} from '@ehop/components'
+import { ehRoot, pkgRoot } from '@ehop/build-utils'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), Components({
+    include: path.resolve(__dirname, '**'),
+    resolvers: [
+      {
+        type: 'component',
+        resolve: (componentName) => {
+          // where `componentName` is always CapitalCase
+          if (componentName.startsWith('Eh'))
+            return { name: componentName.slice(3), from: 'vant' }
+        },
+      },
+    ],
+    dts: false,
+  })],
   resolve: {
     alias: [
       {
-        find: /^element-plus(\/(es|lib))?$/,
-        replacement: path.resolve(epRoot, 'index.ts'),
+        find: /^ehop(\/(es|lib))?$/,
+        replacement: path.resolve(ehRoot, 'index.ts'),
       },
       {
-        find: /^element-plus\/(es|lib)\/(.*)$/,
+        find: /^ehop\/(es|lib)\/(.*)$/,
         replacement: `${pkgRoot}/$2`,
       },
       {
